0a1a75ecdc51841be45cc5d452a093ef7f036ea3
inori_lover <337938188@qq.com>
2021-09-29 14:38:58 +0800

    feat: init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup>
+  import fileImportResult from './HelloWorld.css'
+  import fileWithURLImportResult from './HelloWorld.css?url'
+</script>
+
+<template>
+  expect: '<code>/path/to/file.css</code>'
+  <br />
+  <br />
+  use <code>import fileImportResult from './HelloWorld.css'</code> :
+  <br />
+  <code>{{ fileImportResult }}</code>
+  <br />
+  <br />
+  use <code>import fileWithURLImportResult from './HelloWorld.css?url'</code> :
+  <br />
+  <code>{{ fileWithURLImportResult }}</code>
+  <br />
+</template>
+
